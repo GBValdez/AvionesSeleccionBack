e8f1eeb7a5f5b95548d2b166e7c8f75d0d9eae4b
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-04-28 18:08:31 -0500

    Agregue una clase para los dto y los dto de creacion para los catalogos

--- utils/autoMapperProfiles.cs
+++ utils/autoMapperProfiles.cs
@@ -1,16 +1,17 @@
 ﻿using AutoMapper;
 using AvionesBackNet.Models;
+using AvionesBackNet.Modules.Catalogues;
 using AvionesBackNet.Modules.Categories;
 
 namespace AvionesBackNet.utils
 {
-    public class autoMapperProfiles:Profile
+    public class autoMapperProfiles : Profile
     {
         public autoMapperProfiles()
         {
             //Catalogos
             CreateMap<Catalogo, catalogueDto>();
-            CreateMap<catalogueDto, Catalogo>();
+            CreateMap<catalogueCreationDto, Catalogo>();
 
         }
     }
